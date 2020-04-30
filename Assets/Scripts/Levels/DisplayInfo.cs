05e8727776393fd4b3818df4083099338282d022
brodskiy_andrey <beer_ne@ukr.net>
2020-04-30 15:24:40 +0300

    add display info class

---
+++ Assets/Scripts/Levels/DisplayInfo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+class DisplayInfo : MonoBehaviour
+{
+    [SerializeField] private Text _textDisplayInfo;
+
+    public void TimerLevelDisplay(float timerValue)
+    {
+        if (timerValue >= 0)
+        {
+            _textDisplayInfo.text = timerValue.ToString("0");
+        }
+    }
+}
