26cb05c993d946b088c84d5f5b20612a58679a00
brodskiy_andrey <beer_ne@ukr.net>
2020-05-26 16:38:29 +0300

    fix bug setting panel

---
+++ Assets/Scripts/Game/SettingsButtonView.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+class SettingsButtonView : MonoBehaviour
+{
+    [SerializeField] private GameObject _settingPanel;
+    [SerializeField] private Button _settingButon;
+
+
+    private void Start()
+    {
+        _settingButon.onClick.AddListener(ActivateSettingPanel);
+    }
+
+    private void ActivateSettingPanel()
+    {
+        _settingPanel.SetActive(true);
+    }
+}
